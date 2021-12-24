77c89aba64ff33a1842f25a62c3c7dfdbcc0980a
s2656205 <bnkozdemir@gmail.com>
2021-12-24 12:11:23 +0100

    fixed

--- myapp.py
+++ myapp.py
@@ -5,6 +5,22 @@ from openml_gatheringruns import *
 from OpenML_connection import *
 from PIL import Image
 import plotly.express as px
+import hydralit_components as hc
+from PIL import Image
+
+def running_missingtasks(flows_id, missing_tasks):
+    for i in flows_id:
+        for j in missing_tasks:
+            try:
+                st.write(f"Trying to run flow_id {i} on task {j}")
+                get_flow = openml.flows.get_flow(i, reinstantiate=True)
+                task_missing = openml.tasks.get_task(j)
+                run_missing = openml.runs.run_flow_on_task(flow=get_flow, task=task_missing)
+            except ValueError:
+                st.write(f"flow_id {i} cant run on task {j}")
+            else:
+                st.write("is succesful!")
+
 
 
 
@@ -21,7 +37,8 @@ st.header("Criterias")
 st.write("Please give an input to all of the following:\n  ")
 
 #inputs
-study_id = st.text_input('study_id: (ex. 123)',)
+name = st.text_input("Type here the name of your algorithm",)
+study_id = st.number_input("study_id: (ex. 123)",step=1, value=123, min_value = 0, max_value=100000000)
 flow_id1 = st.number_input("flow_id1: (ex. 7754)",step=1, value=7754, min_value = 0, max_value=100000000)
 flow_id2 = st.number_input("flow_id2: (ex. 7756)",step=1, value=7756, min_value = 0, max_value=100000000)
 
@@ -29,8 +46,33 @@ flows_id = [flow_id1, flow_id2]
 if st.button('Runmodel'):
     #with st.spinner("Training ongoing"):
     missingtasks = gatheringruns(study_id, flow_id1, flow_id2)
+    st.header("The log")
     st.write(f'The missingtasks are:\n {missingtasks}')
     evaluation_id = loadResults(study_id, flows_id)
+
+def plots():
+        st.header("The plots")
+        image = Image.open('barplot.png')
+        st.image(image, caption='barplot')
+        image = Image.open('diagplot.png')
+        st.image(image, caption='diagplot')
+        image = Image.open('splot.png')
+        st.image(image, caption='splot')
+
+expander = st.expander("See all logs")
+with expander:
+    st.write("Here you can see everything that happens")
+    running_missingtasks(flows_id,missingtasks)
+
+plots_expander = st.expander("See the plots")
+with plots_expander:
+        st.header("The plots")
+        image = Image.open('barplot.png')
+        st.image(image, caption='barplot')
+        image = Image.open('diagplot.png')
+        st.image(image, caption='diagplot')
+        image = Image.open('splot.png')
+        st.image(image, caption='splot')
 
 """#    data class
 app = SpotifyApp('spotify_dataset.csv')
@@ -62,15 +104,9 @@ fig = px.scatter(df,
         trendline="lowess")
 #creating a scatter plot 
 if y_axis == 'Highest Charting Position': fig['layout']['yaxis']['autorange'] = "reversed"
-st.plotly_chart(fig)
 
-fig = px.scatter( 
-        x=evaluation_id[0],
-        y=evaluation_id[1],
-        hover_name=evaluation_id[0],
-        title=f'{evaluation_id[0]} compared to {evaluation_id[1]}',
-        color = "Tempo",
-        trendline="lowess")
-#creating a scatter plot 
 """
-print("evaluation_id", evaluation_id)
+
+
+
+
