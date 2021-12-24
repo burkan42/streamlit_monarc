e84b66f9c0906128187d5c5140c78fbaf34b36dc
s2656205 <bnkozdemir@gmail.com>
2021-12-24 03:33:03 +0100

    wee34

--- django-openml/users/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import MultiWidget, TextInput
-
-
-class MyForm(forms.Form):
-	suite = forms.CharField()
-	flow_1 = forms.CharField()
-	flow_2 = forms.CharField()
--- django-openml/users/urls.py
+++
@@ -1,9 +0,0 @@
-#from django.conf.urls import url, include
-from django.urls import include, path
-                                      
-from users.views import dashboard, result
-
-urlpatterns = [
-    path("", dashboard, name="dashboard"),
-    path("result", result, name="result"),
-]
